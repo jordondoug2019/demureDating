afbd9d0f44867d651c351a487d62dba93be719bb
Ubuntu <ubuntu@ip-172-31-49-19.ec2.internal>
2024-08-26 18:59:21 +0000

    Added pseudocode for running total

--- date.py
+++ date.py
@@ -9,14 +9,19 @@ import seafoodMenu
 
 #Welcome Screen 
 print("Welcome to Demure Dating!")
+
 #User inputs who is on the date with them
 userInput= input("Please enter the name of your date")
-print(userInput)
+
+
 #User inputs their budget for the date 
+
 #here, The user budget is automatically converted to a float. This is just in case the user has a budget that isn't a whole number 
 #I used a format string to make the code look cleaner and easier to read.  
 budgetInput= float(input(f"{userInput} is really lucky! What is the budget?"))
-print(budgetInput)
+
+
+#Print the restaurant menu (your group created this!) 
 
 #prints restuarant menu using a nested for loop. The loop goes through each key in the dictionary, 
 # and for each key, it iterates over the corresponding values.
@@ -38,3 +43,17 @@ for key in seafoodMenu.Menu["Dessert"]:
     print (key + "\n")
     for value in seafoodMenu.Menu["Dessert"][key]:
         print (value + "\n")
+
+
+#User inputs their food/drink item choices from a restaurant menu list (for themselves and their date)
+userChoice=input("What will you be having for Dinner?")
+print(userChoice)
+
+userDateChoice=input("What will your date have for dinner")
+
+#Script tells the user how much money they have left after each order.
+
+#Take user menu choice and date menu choice 
+#Get corresponding prices for each meal and subtract total from budget 
+#Budget - (userChoice + UserDateChoice)
+#need conditonals
