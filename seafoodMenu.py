dbd3681a425fe9f22dd80781c7c6b8d594189b9c
Ubuntu <ubuntu@ip-172-31-49-19.ec2.internal>
2024-08-26 19:07:24 +0000

    Changed seafood menu prices

--- seafoodMenu.py
+++ seafoodMenu.py
@@ -9,21 +9,23 @@
 #appDictionary
 Menu={
         "AppMenu": {
-                 "HushPuppies": ["price: 8.00", "Description: fried corn meal fritters", "Vegan: Yes"],
-                 "Clam Chowder": ["price: 13.00", "Description: New England Style Clam Chowder", "Vegan: No"],
-                 "Shrimp Cocktail": ["price: 10.00", "Description: Steamed Jumbo Shrimp with cocktail sauce", "Vegan: No"],
+                 "HushPuppies": [8.00, "Description: fried corn meal fritters", "Vegan: Yes"],
+                 "Clam Chowder": [13.00, "Description: New England Style Clam Chowder", "Vegan: No"],
+                 "Shrimp Cocktail": [10.00, "Description: Steamed Jumbo Shrimp with cocktail sauce", "Vegan: No"],
             },
 
         "Entree":{
-            "Snow Crab Boil": ["price: 25.00", "Description: 1lb of snow crab with corn and potatoes", "Vegan: No"],
-            "Lobster Linguine": ["price: 30.00", "Description:Buttered Lobster in a cream sauce in Linguine with a bed of steamed broccoli", "Vegan: No"],
-            "Whole Maine Lobster": ["price: MP", "Description: 1 1/2lb of lobster with baked potato and mixed veggies", "Vegan: No"],      
+            "Snow Crab Boil": [25.00, "Description: 1lb of snow crab with corn and potatoes", "Vegan: No"],
+            "Lobster Linguine": [30.00, "Description:Buttered Lobster in a cream sauce in Linguine with a bed of steamed broccoli", "Vegan: No"],
+            "Whole Maine Lobster": [100.00, "Description: 1 1/2lb of lobster with baked potato and mixed veggies", "Vegan: No"],      
         },
 
         "Dessert": {
-            "Canoli": ["price: 5.00", "Description:Marscapone cheese with chocolate chips in a fried pastry shell", "Vegan: No"],
-            "Apple Pie": ["price: 8.00"," Description: Macintosh Apples and Cinnamon in a flakey buttery crust with a scoop of vanilla bean ice cream on top" , "Vegan: Yes"],
-            "Mochi Ice Cream": ["price: 6.00", "Description: Vanilla with Red bean", "Vegan:No"],
+            "Canoli": [5.00, "Description:Marscapone cheese with chocolate chips in a fried pastry shell", "Vegan: No"],
+            "Apple Pie": [8.00," Description: Macintosh Apples and Cinnamon in a flakey buttery crust with a scoop of vanilla bean ice cream on top" , "Vegan: Yes"],
+            "Mochi Ice Cream": [6.00, "Description: Vanilla with Red bean", "Vegan:No"],
         
         } 
 }
+
+print(sum(Menu["AppMenu"].values))
